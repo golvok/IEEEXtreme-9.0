8b55e5ac25d50a6b167b86455450e9374f71a30b
Matthew J.P. Walker <walkerm930@gmail.com>
2015-10-24 12:41:35 -0400

    zoom in: fix space handling & add space test case

--- zoom_in.cpp
+++ zoom_in.cpp
@@ -41,6 +41,7 @@ int main() {
 	std::unordered_map<char,std::vector<std::string>> zoomed_in_chars;
 
 	for (uint i = 0; i < num_chars; ++i) {
+		std::cin.get();
 		char c = std::cin.get();
 		std::vector<std::string> zoomed_in_c;
 		for (uint row = 0; row < num_rows; ++row) {
@@ -50,25 +51,26 @@ int main() {
 				zoomed_in_c.back().push_back(std::cin.get());
 			}
 		}
-        std::cout << "read " << c << '\n';
+        // std::cout << "read " << c << '\n';
 		zoomed_in_chars.emplace(c,std::move(zoomed_in_c));
 	}
 
-	auto num_strs = get<char>();
-    std::cout << num_strs << " 1\n";
-    // std::cin.get();
-    std::cout << "2\n";
-	for (uint i = 0; i < num_strs; ++i) {
-        std::cout << "3\n";
+	auto num_strs = get<uint>();
+    // std::cout << num_strs << " 1\n";
+    std::cin.get();
+    // std::cout << "2\n";
+	for (uint i = 0; i < (uint)num_strs; ++i) {
+		if (std::cin.eof()) break;
+        // std::cout << "3\n";
         std::string str;
         while (true) {
-            std::cout << "4\n";
+            // std::cout << "4\n";
             char c = std::cin.get();
-            std::cout << c;
-            if ( c == '\n') break;
+            // std::cout << c;
+            if ( c == '\n' || std::cin.eof()) break;
             str.push_back(c);
         }
-        std::cout << "str=" << str << '\n';
+        // std::cout << "str=" << str << '\n';
 		for (uint row = 0; row < num_rows; ++row) {
 			for (char c : str) {
                 std::cout << zoomed_in_chars[c][row]; 
