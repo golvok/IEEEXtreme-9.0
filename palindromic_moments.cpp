979912d3fd6f94fa2041fe9881f12721ac4f2b7b
Matthew J.P. Walker <walkerm930@gmail.com>
2015-10-24 00:53:10 -0400

    add palindromic moments parsing code

---
+++ palindromic_moments.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cmath>
+#include <cstdint>
+#include <cstdlib>
+#include <cstdio>
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <stdexcept>
+#include <sstream>
+#include <unordered_map>
+
+template<typename T, typename STREAM>
+T get(STREAM& is) {
+	T t;
+	is >> t;
+	return t;
+}
+
+struct print_printable { };
+
+template<typename T, typename STREAM>
+auto operator<<(STREAM& os, const T& t) -> decltype(static_cast<const print_printable*>(&t),os) {
+	t.print(os);
+	return os;
+}
+
+int main() {
+	auto num_tests = get<uint>(std::cin);
+	std::vector<uint> years;
+	for (uint i = 0; i < num_tests; ++i) {
+		years.push_back(get<uint>(std::cin));
+	}
+
+	return 0;
+}
